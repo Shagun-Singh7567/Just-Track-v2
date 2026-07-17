ac23e8d1b4a4a26de5e21093e775644b94fae0e6
Shagun Singh <shagundun@gmail.com>
2026-07-17 22:42:17 +0530

    Corrected Request Mapping in backend

--- src/main/java/com/shagun/just_track_v2/controllers/TransactionController.java
+++ src/main/java/com/shagun/just_track_v2/controllers/TransactionController.java
@@ -16,8 +16,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
+@RequestMapping("/api")
 @CrossOrigin(origins = {"http://localhost:5173", "https://just-track-v2.vercel.app"})
 public class TransactionController {
     @Autowired
