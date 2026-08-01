d4eee4f81a7da3c0e76e45fd345f39ed8f73f363
Shagun Singh <shagundun@gmail.com>
2026-08-01 20:18:15 +0530

    Fixed security config

--- src/main/java/com/shagun/just_track_v2/config/SecurityConfig.java
+++ src/main/java/com/shagun/just_track_v2/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.shagun.just_track_v2.security.JwtAuthFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -37,6 +38,7 @@ public class SecurityConfig {
             .csrf(csrf -> csrf.disable())
             .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(auth -> auth
+                .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                 .requestMatchers("/auth/**", "/error").permitAll()
                 .anyRequest().authenticated()
             )
